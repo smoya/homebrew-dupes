156214547b9b9d367d3dd7fcfbdc7270b6614b86
Sergio Moya <smoya89@gmail.com>
2013-06-10 12:43:47 +0300

    Update ant.rb

--- ant.rb
+++ ant.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Ant < Formula
   homepage 'http://ant.apache.org/'
-  url 'http://www.apache.org/dyn/closer.cgi?path=ant/binaries/apache-ant-1.9.1-bin.tar.gz'
+  url 'http://apache.rediris.es//ant/binaries/apache-ant-1.9.1-bin.tar.gz'
   sha1 '75942a23d7a9511db32c51f935e923d7c0878f7e'
 
   def install
